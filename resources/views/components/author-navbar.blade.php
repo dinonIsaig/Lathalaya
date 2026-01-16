8ba223084617b8a5a9b31096373f47ce3769c896
Gwen <lee.gwyneth18@gmail.com>
2026-01-16 14:38:14 +0800

    style(nav bar): changed icon for sign out button

--- resources/views/components/author-navbar.blade.php
+++ resources/views/components/author-navbar.blade.php
@@ -29,7 +29,7 @@
 
             <button class=" text-neutral-gray hover:text-primary transition-colors p-1">
                 <svg class="icons" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7"></path>
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"></path>
                 </svg>
             </button>
         </div>
