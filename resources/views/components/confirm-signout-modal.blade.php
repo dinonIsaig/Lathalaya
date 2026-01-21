1bce1080f6db1b4ddba684cb0d0074f48324512b
Dinon Isaig <dinonbonagua@gmail.com>
2026-01-22 02:40:24 +0800

    fix: fix the cancel in sign out modal

--- resources/views/components/confirm-signout-modal.blade.php
+++ resources/views/components/confirm-signout-modal.blade.php
@@ -1,4 +1,4 @@
-@props(['id' => 'confirmSignOutModal', 'type',])
+@props(['id' => 'confirmSignOutModal', 'type'])
 
 <div id="{{ $id }}" class="fixed inset-0 flex items-center justify-center bg-black/50 z-50 hidden">
 
@@ -13,23 +13,21 @@
             <p class="text-gray-600 mb-10 leading-relaxed">
                 Signing out will end your session. Are you sure you want to sign out?
             </p>
-
-            <form action="{{ route($type . '.sign-out') }}" method="POST">
-            @csrf
-                <div class="flex gap-4">
-                    <button
-                        onclick="document.getElementById('{{ $id }}').classList.add('hidden')"
-                        class="cancel-btn">
-                        Cancel
-                    </button>
-
+            <div class="flex gap-4">
+                <button
+                    onclick="document.getElementById('{{ $id }}').classList.add('hidden')"
+                    class="cancel-btn">
+                    Cancel
+                </button>
+                <form action="{{ route($type . '.sign-out') }}" method="POST" class="w-full">
+                @csrf
                     <button
                         type="submit"
                         class="normal-btn w-full">
                         Sign Out
                     </button>
-                </div>
-            </form>
+                </form>
+            </div>
         </div>
     </div>
 
