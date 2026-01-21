6b384328372896252dba7aa98d1f7953e2d0a611
Dinon Isaig <dinonbonagua@gmail.com>
2026-01-22 01:35:45 +0800

    refactor: landing page fix

--- resources/views/lathalaya.blade.php
+++ resources/views/lathalaya.blade.php
@@ -19,7 +19,7 @@
                         <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/>
                         <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"/>
                     </svg>
-                    <a href="{{ route('author.register') }}" class="hidden sm:inline-block ml-1 tracking-tight text-base">Create Article</a>
+                    <a href="{{ route('author.sign-in') }}" class="hidden sm:inline-block ml-1 tracking-tight text-base">Create Article</a>
                 </button>
             </div>
 
