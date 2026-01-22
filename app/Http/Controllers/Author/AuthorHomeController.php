15fa576794aeed785814ef85444a536d6da85246
Joyrel <baladjayjoyrelg20@gmail.com>
2026-01-23 06:00:13 +0800

    feat(controllers): added controllers handling filter

--- app/Http/Controllers/Author/AuthorHomeController.php
+++ app/Http/Controllers/Author/AuthorHomeController.php
@@ -4,16 +4,16 @@ namespace App\Http\Controllers\Author;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\View\View;
 use App\Models\Article;
+use App\Filters\ArticleFilter;
 
 class AuthorHomeController extends Controller
 {
     public function index(Request $request)
     {
-
-        $articles = Article::where('status', 'Published')->latest()->take(9)->get();
-
+        $query = Article::where('status', 'Published');
+        ArticleFilter::apply($query, $request);
+        $articles = $query->latest()->take(9)->get();
         $header = Article::where('status', 'Published')->latest()->first();
 
         return view('author.home', [
--- app/Http/Controllers/LathalayaController.php
+++ app/Http/Controllers/LathalayaController.php
@@ -3,23 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\View\View;
 use App\Models\Article;
-
+use App\Filters\ArticleFilter; // 1. Added missing import
 
 class LathalayaController extends Controller
 {
     public function index(Request $request)
     {
-        $articles = Article::where('status', 'Published')->latest()->take(9)->get();
 
-        $header = Article::where('status', 'Published')->latest()->first();
+        $query = Article::where('status', 'Published');
+        ArticleFilter::apply($query, $request);
+        $header = (clone $query)->latest()->first();
+        $articles = $query->latest()
+            ->when($header, function ($q) use ($header) {
+                return $q->where('article_id', '!=', $header->article_id);
+            })
+            ->take(9)
+            ->get();
 
         return view('lathalaya', [
             'publishedArticles' => $articles,
             'headerArticle' => $header
         ]);
     }
-
-
 }
